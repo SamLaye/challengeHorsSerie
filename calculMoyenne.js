4e3bd99fb5aa70e0fb7201b69a43ded1f7d81c71
Sam Laye <layesamss97@gmail.com>
2023-07-11 19:05:16 +0000

    Suppression de la console.log

--- calculMoyenne.js
+++ calculMoyenne.js
@@ -1,6 +1,5 @@
 let tab = []
 
-
 function addToTab(){
 
 	let valeurSaisie = document.getElementById("input");
@@ -10,17 +9,11 @@ function addToTab(){
 		tab.push(valeurSaisie.value)
 		tab = tab.map(num => parseFloat(num))
 		valeurSaisie.value = ''
-		console.log(tab)
 		document.getElementById('tableau').innerText = tab + " ";
 	}else{
 		alert('Veuillez saisir un nombre!')
 	}
-
-	
-
 }
-
- 
 
 function calculMoyenne(){
 
@@ -32,6 +25,4 @@ function calculMoyenne(){
 	document.getElementById("laSomme").innerText = "La somme des éléments du tableau est: " + somme;
 	document.getElementById("laTaille").innerText = "Le tableau a une taille de: " + tab.length;
 	document.getElementById("calcul").innerText = "Moyenne: " + somme + " : " + tab.length + " = " + moyenne;
-
-	// document.write(moyenne);
 }
